5daf2bdfd76e0f9113cb689bd5f298b93c108b68
MsJ <159886481+Boholjonna@users.noreply.github.com>
2025-05-24 11:11:54 +0800

    Refactor: convert stock subpane to popup pane

--- src/main/java/sold_stocks/soldStock.java
+++ src/main/java/sold_stocks/soldStock.java
@@ -1,32 +1,29 @@
 package sold_stocks;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
-public class soldStock extends Application {
-    @Override
-    public void start(@NotNull Stage stage) throws IOException {
-        // Set icon (taskbar, window bar)
-        stage.getIcons().add(new javafx.scene.image.Image(getClass().getResource("/images/logo.png").toExternalForm()));
-        FXMLLoader fxmlLoader = new FXMLLoader(soldStock.class.getResource("/soldStocks/soldstock_form.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 377, 413);
-        scene.setFill(Color.TRANSPARENT); // removes white background
-        stage.setTitle("Sold stocks");
-        stage.setScene(scene);
-        stage.initStyle(StageStyle.TRANSPARENT); // <--- this removes the top bar
-        stage.show();
-        System.out.println("Java version: " + System.getProperty("java.version"));
-        System.out.println("JavaFX version: " + System.getProperty("javafx.version"));
-    }
 
-    public static void main(String[] args) {
-        launch();
+public class soldStock {
+
+    public void showPopup() throws IOException {
+        Stage stage = new Stage();
+
+        stage.getIcons().add(new javafx.scene.image.Image(getClass().getResource("/images/logo.png").toExternalForm()));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/soldStocks/soldstock_form.fxml"));
+        Scene scene = new Scene(loader.load(), 377, 432);
+        scene.setFill(Color.TRANSPARENT);
+        stage.setScene(scene);
+        stage.setTitle("Add or Edit Product");
+        stage.initStyle(StageStyle.TRANSPARENT); // or UNDECORATED if preferred
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setResizable(false);
+        stage.showAndWait();
     }
 }
