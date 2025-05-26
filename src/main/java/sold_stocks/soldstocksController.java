5ef239461040d61c8dca8d42b29e8cfab971c550
Joanne R. Georfo <georfo.joanne05@gmail.com>
2025-05-27 01:03:56 +0800

    Buttons: recconnected

--- src/main/java/sold_stocks/soldstocksController.java
+++ src/main/java/sold_stocks/soldstocksController.java
@@ -2,20 +2,18 @@ package sold_stocks;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 public class soldstocksController {
+
     @FXML
     private Pane sold_pane;
 
     @FXML
     private void Exit() {
-        // Hide the pane first (optional if you remove it)
-        // Get the current stage from the add_pane
-        Stage stage = (Stage) sold_pane.getScene().getWindow();
-
-        // Close the stage (window)
-        stage.close();
+        if (sold_pane.getParent() instanceof Pane parent) {
+            parent.getChildren().remove(sold_pane); // Only removes the subpane
+        }
     }
 
 }
+
